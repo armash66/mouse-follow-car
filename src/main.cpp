3fcee091c8e1b1613d3dec19f2b27fd6346fe350
Ansari Armash <ansariarmash007@gmail.com>
2026-02-16 20:36:10 +0530

    implement basic mouse-following square using linear interpolation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <SDL2/SDL.h>
+#include <cmath>
 
 int main(int argc, char* argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        return 1;
-    }
+    SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = SDL_CreateWindow(
-        "Mouse Follow Car",
+        "Mouse Follower",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800,
@@ -15,26 +14,52 @@ int main(int argc, char* argv[])
         SDL_WINDOW_SHOWN
     );
 
-    if (!window) {
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
 
     bool running = true;
     SDL_Event event;
 
+    // Shape position (center of screen initially)
+    float posX = 400.0f;
+    float posY = 300.0f;
+
+    int size = 20;      // Square size
+    float speed = 0.05f; // How fast it follows mouse (0 to 1)
+
     while (running)
     {
+        // ---- Handle Events ----
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_QUIT)
                 running = false;
         }
 
+        // ---- Get Mouse Position ----
+        int mouseX, mouseY;
+        SDL_GetMouseState(&mouseX, &mouseY);
+
+        // ---- Move Toward Mouse (Linear Interpolation) ----
+        posX += (mouseX - posX) * speed;
+        posY += (mouseY - posY) * speed;
+
+        // ---- Render ----
         SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
         SDL_RenderClear(renderer);
+
+        SDL_Rect square;
+        square.w = size;
+        square.h = size;
+        square.x = static_cast<int>(posX - size / 2);
+        square.y = static_cast<int>(posY - size / 2);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &square);
+
         SDL_RenderPresent(renderer);
     }
 
